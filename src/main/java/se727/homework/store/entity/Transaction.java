5d05337936f863c2ccafdf788ee52b417dc1a0c4
Jiramed <jiramed_wi@cmu.ac.th>
2021-09-03 18:07:51 +0700

    Add constructor and Hahcode for Transaction class

--- src/main/java/se727/homework/store/entity/Transaction.java
+++ src/main/java/se727/homework/store/entity/Transaction.java
@@ -1,9 +1,35 @@
 package se727.homework.store.entity;
 
+import java.util.Objects;
+
 public class Transaction {
     Product product;
     Customer customer;
     Double salePrice;
+
+    public Transaction(Product product, Customer customer, Double salePrice) {
+        this.product = product;
+        this.customer = customer;
+        this.salePrice = salePrice;
+    }
+
+    public Transaction() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(product, that.product) &&
+                Objects.equals(customer, that.customer) &&
+                Objects.equals(salePrice, that.salePrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, customer, salePrice);
+    }
 
     public Product getProduct() {
         return product;
