6ba18c7aadb383416096d2ee1cace8b2ccc6ea60
Jiramed <jiramed_wi@cmu.ac.th>
2021-09-03 18:06:55 +0700

    add new constructor and hashcode equal on customer class

--- src/main/java/se727/homework/store/entity/Customer.java
+++ src/main/java/se727/homework/store/entity/Customer.java
@@ -1,8 +1,31 @@
 package se727.homework.store.entity;
 
+import java.util.Objects;
+
 public class Customer {
     Long id;
     String customerName;
+
+    public Customer(String customerName) {
+        this.customerName = customerName;
+    }
+
+
+    public Customer() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(customerName, customer.customerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerName);
+    }
 
     public Long getId() {
         return id;
