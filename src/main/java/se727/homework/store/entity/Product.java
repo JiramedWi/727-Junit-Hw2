4eb2867187516406c3181db2798665d02c15d717
Jiramed <jiramed_wi@cmu.ac.th>
2021-09-03 16:07:05 +0700

    Add hashcode() and constructor(both) in ProductService

--- src/main/java/se727/homework/store/entity/Product.java
+++ src/main/java/se727/homework/store/entity/Product.java
@@ -1,9 +1,33 @@
 package se727.homework.store.entity;
 
+import java.util.Objects;
+
 public class Product {
     String productId;
     String productName;
     Double StandardPrice;
+
+    public Product(String productName, Double standardPrice) {
+        this.productName = productName;
+        StandardPrice = standardPrice;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productName.equals(product.productName) &&
+                StandardPrice.equals(product.StandardPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, StandardPrice);
+    }
 
     public String getProductId() {
         return productId;
